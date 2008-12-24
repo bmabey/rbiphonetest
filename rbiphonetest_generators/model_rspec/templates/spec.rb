84c4cacf2e6e600555c049d5db5db7b8f306721d
Ben Mabey <ben@benmabey.com>
2008-12-23 21:59:13 -0700

    nest rspec example groups in OSX modules to avoid fully qualifying it everytime

--- rbiphonetest_generators/model_rspec/templates/spec.rb
+++ rbiphonetest_generators/model_rspec/templates/spec.rb
@@ -1,12 +1,15 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 require "<%= class_name %>.bundle"
-OSX::ns_import :<%= class_name %>
 
-describe OSX::<%= class_name %> do
+module OSX
+  ns_import :<%= class_name %>
+
+  describe <%= class_name %> do
   
-  it "should exist" do
-    OSX::<%= class_name %>
-  end
-  
+    it "should exist" do
+      <%= class_name %>
+    end
+
+  end 
 end
